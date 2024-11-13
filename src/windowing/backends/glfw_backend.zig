ac76fd6651ea255501da790d4cfeb1ac4381642b
Thubie de Jong <thubie@Thubies-MacBook-Pro.local>
2024-11-13 20:44:12 +0100

    Refactor the glfw backend for windowing

--- src/windowing/backends/glfw_backend.zig
+++ src/windowing/backends/glfw_backend.zig
@@ -1,36 +1,52 @@
+const std = @import("std");
+const assert = std.debug.assert;
+
 const glfw = @import("../../c.zig");
 
-pub const Window = struct {
-    window: *glfw.GLFWwindow,
+var window: ?*glfw.GLFWwindow = null;
 
-    pub fn init(width: u32, height: u32, title: [:0]const u8) !Window {
-        if (glfw.glfwInit() != glfw.GLFW_TRUE) return error.WindowCreationFailed;
-        glfw.glfwWindowHint(glfw.GLFW_CLIENT_API, glfw.GLFW_NO_API);
+pub fn init(width: u32, height: u32, title: [:0]const u8) !void {
+    if (glfw.glfwInit() != glfw.GLFW_TRUE) return error.WindowCreationFailed;
+    glfw.glfwWindowHint(glfw.GLFW_CLIENT_API, glfw.GLFW_NO_API);
 
-        const glfw_window = glfw.glfwCreateWindow(
-            @intCast(width),
-            @intCast(height),
-            title,
-            null,
-            null,
-        ) orelse return error.WindowCreationFailed;
+    const glfw_window = glfw.glfwCreateWindow(
+        @intCast(width),
+        @intCast(height),
+        title,
+        null,
+        null,
+    ) orelse return error.WindowCreationFailed;
 
-        return .{
-            .window = glfw_window,
-        };
+    window = glfw_window;
+}
+
+pub fn deinit() void {
+    assert(window != null);
+
+    glfw.glfwDestroyWindow(window);
+    glfw.glfwTerminate();
+}
+
+pub fn windowClosed() bool {
+    assert(window != null);
+
+    return glfw.glfwWindowShouldClose(window) == glfw.GLFW_TRUE;
+}
+
+pub fn update() void {
+    assert(window != null);
+
+    glfw.glfwPollEvents();
+}
+
+pub fn getVulkanExtensions(allocator: std.mem.Allocator) std.ArrayList {
+    assert(window != null);
+
+    var vk_exts_count: u32 = 0;
+    const vk_exts = glfw.glfwGetRequiredInstanceExtensions(&vk_exts_count);
+
+    var vk_exts_list = try std.ArrayList([*:0]const u8).initCapacity(allocator, vk_exts_count);
+    for (0..vk_exts_count) |i| {
+        try vk_exts_list.append(vk_exts[i]);
     }
-
-    pub fn deinit(self: *const Window) void {
-        glfw.glfwDestroyWindow(self.window);
-        glfw.glfwTerminate();
-    }
-
-    pub fn windowClosed(self: *const Window) bool {
-        return glfw.glfwWindowShouldClose(self.window) == glfw.GLFW_TRUE;
-    }
-
-    pub fn update(self: *const Window) void {
-        _ = self;
-        glfw.glfwPollEvents();
-    }
-};
+}
--- src/windowing/windowing.zig
+++ src/windowing/windowing.zig
@@ -1,31 +1,25 @@
 const std = @import("std");
-const assert = std.debug.assert;
 
-const Backend = @import("./backends/glfw_backend.zig").Window;
-
-var backend: ?Backend = null;
+const Backend = @import("./backends/glfw_backend.zig");
 
 pub const Window = struct {
-    pub fn init(width: u32, height: u32, title: [:0]const u8) !Window {
-        if (backend == null) {
-            backend = try Backend.init(width, height, title);
-        }
-
-        return .{};
+    pub fn init(width: u32, height: u32, title: [:0]const u8) !void {
+        try Backend.init(
+            width,
+            height,
+            title,
+        );
     }
 
     pub fn deinit() void {
-        assert(backend != null);
-        backend.?.deinit();
+        Backend.deinit();
     }
 
     pub fn windowClosed() bool {
-        assert(backend != null);
-        return backend.?.windowClosed();
+        return Backend.windowClosed();
     }
 
     pub fn update() void {
-        assert(backend != null);
-        backend.?.update();
+        Backend.update();
     }
 };
